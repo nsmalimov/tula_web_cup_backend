9eb4b9d8ad330986a0e2f8f083e5d5c7b7c3b667
Nurislam Alimov <nalimov@sardinasystems.com>
2019-04-12 11:51:17 +0300

    get user by token

--- repositories/db_repository/users.go
+++ repositories/db_repository/users.go
@@ -2,6 +2,7 @@ package db_repository
 
 import (
 	"github.com/jmoiron/sqlx"
+	"log"
 )
 
 type DbUser struct {
@@ -18,4 +19,18 @@ func (b *DbUsersRepository) Create(userToken string) error {
 	_, err := b.DB.Query(query, userToken)
 
 	return err
+}
+
+func (b *DbUsersRepository) GetUserByToken(userToken string) (*DbUser, error) {
+	var dbUser DbUser
+
+	query := `INSERT INTO users (token) VALUES ($1)`
+
+	err := b.DB.Get(dbUser, query, userToken)
+
+	if err != nil {
+		log.Println(err)
+	}
+
+	return &dbUser, err
 }
